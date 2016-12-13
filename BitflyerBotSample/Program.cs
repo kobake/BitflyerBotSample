0816ec8d205e37769e822ac7354c2be2bb19778d
kobake <kobake@users.sourceforge.net>
2016-12-13 20:12:21 +0900

    相場の変動が大きく変動したときに売買を行う例

--- BitflyerBotSample/Program.cs
+++ BitflyerBotSample/Program.cs
@@ -34,9 +34,46 @@ namespace BitflyerBotSample
             }).Wait(); // タスクが終わるまで待つ
         }
 
+        // 相場の変動が大きく変動したときに売買を行うボット
         static async Task BotLogic()
         {
-            // 5秒毎にランダムに買い注文、売り注文を出し続ける
+            // 1分毎にチェック
+            double lastPrice = 0;
+            while (true)
+            {
+                // 板情報を取得
+                var board = await client.GetBoard();
+                double currentPrice = board.MiddlePrice; // 現在の中間価格
+                Console.WriteLine("current = " + currentPrice);
+
+                // 前回の相場と比較する
+                if (lastPrice != 0)
+                {
+                    double gap = board.MiddlePrice - lastPrice;
+                    // 1分間で100円値下がりしていたら買い時とみて買う
+                    if (gap <= -100)
+                    {
+                        Console.WriteLine("buy");
+                        await client.Buy(0.001);
+                    }
+                    // 1分間で100円値上がりしていたら売り時とみて売る
+                    else if (gap >= 100)
+                    {
+                        Console.WriteLine("sell");
+                        await client.Sell(0.001);
+                    }
+                }
+                lastPrice = currentPrice;
+
+                // 1分待機
+                Console.WriteLine("wait");
+                await Task.Delay(60 * 1000);
+            }
+        }
+
+        // 5秒毎にランダムに買い注文、売り注文を出し続けるボット
+        static async Task BotLogicRandom()
+        {
             Random r = new Random();
             while (true)
             {
